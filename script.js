3084c6f536b7f0de3f5f9d6d15ed63c928834b5b
Imre <rabl.imre95@gmail.com>
2024-07-03 18:56:54 +0200

    added characters

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-console.log('hello world');
-
 async function fetchData() {
 const fetchResult = await fetch("https://swapi.dev/api/people/");
 console.log(fetchResult);
 const data = await fetchResult.json();
-console.log(data);
+const characters = data.results; //characters = array[10]
+
+const rootElement = document.querySelector("#root");
+let charactersHtml = "";
+
+// for (let i = 0; i < characters.length; i++) {
+//     console.log(characters[i]);
+
+//     charactersHtml += `
+//         <div class="character">
+//         <p class="name"> ${characters[i].name} </p>
+//         <p class="height"> ${characters[i].height} cm</p>
+//         <p class="mass"> ${characters[i].mass} kg </p>
+//         </div>
+//     `;
+// }
+
+characters.forEach(() => {
+    charactersHtml += `
+    <div class="character">
+    <p class="name"> ${character.name} </p>
+    <p class="height"> ${character.height} cm </p>
+    <p class="mass"> ${character.mass} kg </p>
+    </div>
+    `
+});
+
+rootElement.insertAdjacentElement("beforeend",charactersHtml);
+
 //mindenhol elérhető a data a fetchData függvényen belül
 }
 
