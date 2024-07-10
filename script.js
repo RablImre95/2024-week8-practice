83b34f9ee5ab3189e5ef4620fcd1a8ab7daa099e
Imre <rabl.imre95@gmail.com>
2024-07-10 17:56:27 +0200

    updating code and adding comments

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let charactersData = [];
 
-const characterComponent = (name, height, mass) => `
-        <div class="character">
-        <h2> character: </h2>
-        <p class="name"> ${name} </p>
-        <p class="height"> ${height}cm </p>
-        <p class="mass"> ${mass}kg </p>
-      </div>
-  `;
-
-const charactersComponent = (charactersData) => `
-  <div class="characters">
-  ${charactersData.map((characterData, index) => characterComponent(characterData.name, characterData.height, characterData.mass, index)).join(" ")}
+const characterComponent = (name, height, mass, index) => `
+  <div class="character">
+    <h2> character ${index + 1}: </h2>
+    <p class="name"> ${name} </p>
+    <p class="height"> ${height}cm </p>
+    <p class="mass"> ${mass}kg </p>
   </div>
 `;
-
+//JS primitívből elkészíti nekünk a HTML kódot a komponens, a karakter és a karakterek is, kap bemenő paramétert és körbeöleli HTML kóddal
+const charactersComponent = (charactersData) => `
+  <div class="characters">
+    ${charactersData
+    .map((characterData, index) => characterComponent(characterData.name, characterData.height, characterData.mass, index))
+    .join(" ")
+  }
+  </div>
+`;
+// ez a kód csinálja a logikát, js objektumokat összehozza a map egy nagy stringgé, minden egyes elemén a tömbnek lefut a callback függvény és visszatér vele egy stringgel (a map alapvető visszatérése egy tömb) 
 const fetchData = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
@@ -22,22 +25,22 @@ const fetchData = async (url) => {
 }
 
 const makeDomFromData = (data, rootElement) => {
-  charactersData.push(...data.results);
+  charactersData.push(...data.results); //megkapott adat results értéket hozzáadjuk az elején deklarált tömbhöz
   let charactersHtml = charactersComponent(charactersData);
   const buttonHtml = `<button class="fetch">load more...</button>`;
 
-  rootElement.insertAdjacentHTML("beforeend",charactersHtml);
-  if (data.next) {
-    rootElement.insertAdjacentHTML("beforeend",buttonHtml);
+  rootElement.insertAdjacentHTML("beforeend", charactersHtml); //belépési pontjába a weboldalnak adja hozzá a karaktert
+  if (data.next) { //gomb hozzáadása a weoboldalhoz
+    rootElement.insertAdjacentHTML("beforeend", buttonHtml);
 
-  const buttonElement = document.querySelector("button.fetch")
-  buttonElement.addEventListener("click", async () => {
-    buttonElement.innerText = "loading next page...";
-    buttonElement.disabled= true;
+    const buttonElement = document.querySelector("button.fetch");
+    buttonElement.addEventListener("click", async () => { //gomb kap egy eseményfigyelőt, kap egy klikk eseményt és kap egy callback függvényt
+      buttonElement.innerText = "loading next page...";
+      buttonElement.disabled = true;
 
-    const newData = await fetchData(data.next);
-    rootElement.innerHTML = ""
-    makeDomFromData(newData, rootElement);
+      const newData = await fetchData(data.next);
+      rootElement.innerHTML = ""; //weboldal belépési pontjának a tartalma legyen egy üres string, amikor befejeződik a fetch, beilleszti az új tartalmat a régit pedig nem tölti be újra
+      makeDomFromData(newData, rootElement);
     });
   }
 }
@@ -49,6 +52,7 @@ const init = async () => {
 }
 
 init();
+
 
 /*async function fetchData() {
     const fetchResult = await fetch("https://swapi.dev/api/people/");
